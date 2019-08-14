15c4e8c080ad9e56d00a2ddd2f01b336bc90187f
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-08-13 19:27:54 -0700

    finish up function fill_dates( )

--- R/fill_dates.R
+++ R/fill_dates.R
@@ -1,7 +1,23 @@
-fill_dates <- function(df, date_id_col, start_date, end_date){
+#' Fill in missing dates in a dataframe with intermittent dates
+#'
+#' @param df a dataframe where at least 1 column has dates
+#' @param date_col name of the column that has dates
+#' @param start_date start date, e.g. "2019-01-01"
+#' @param end_date end date, e.g. "2019-02-01"
+#'
+#' @return the dataframe you started with, expanded to include all missing dates as rows
+#' @export
+#'
+#' @examples
+#' df <- data.frame(dates = c("2019-01-01", "2019-02-01"), numbers = c(3,4))
+#' df %>% fill_dates(dates, "2019-01-01", "2019-02-08")
+fill_dates <- function(df,
+                       date_col,
+                       start_date,
+                       end_date){
   # inputs:
   # df is a dataframe
-  # date_id_col is a column with date ids - e.g. "20190101"
+  # date_col is the name of a column with date ids (e.g. "20190101")
   # start_date and end_date create the range of dates to "fill in"
 
   # create date range:
@@ -11,14 +27,14 @@ fill_dates <- function(df, date_id_col, start_date, end_date){
 
   # "quote"/"capture" the value that the user inputs (the actual arg),
   # but don't evaluate it:
-  date_id_col <- enexpr(date_id_col)
+  date_col <- rlang::enexpr(date_col)
 
   # now join:
   df <-
     df %>%
-    mutate(dates_fill = ymd(!!date_id_col)) %>%
-    full_join(df_full_dates) %>%
-    arrange(dates_fill)
+    dplyr::mutate(dates_fill = lubridate::ymd(!!date_col)) %>%
+    dplyr::full_join(df_full_dates) %>%
+    dplyr::arrange(dates_fill)
 
   return(df)
 
